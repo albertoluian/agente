f1fd64e8a20624eab5c4a41de8ca22f8de25b684
Vanessa Carvalho Lima <73398589+Vnscarli@users.noreply.github.com>
2023-09-25 18:08:52 -0300

    Update main.py
    Comentários na main

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
+#Importamos as bibliotecas e arquivos necessários
 from Agent import Agent
 import copy
 import os
 import numpy as np
 from time import sleep
 from threading import Thread
+#Cria uma lista para armazenar a pontuação de cada teste feito
 maxEncountered = []
-n_threads = 16
+
 def runAgent(n):
+    #Serve para rodar o código para vários agentes
     global maxEncountered
     points = []
     for i in range(n):
+        #Define as escolhas e restrições do agente, juntamente com a percepção inicial
         agent = Agent(choices=["noOp", "left", "right", "up", "down", "asp"], percepts=None, restrictions=[("left", "wallLeft"), ("right", "wallRight"), ("down", "wallDown"), ("up", "wallUp"), ("asp", "clean")])
 
         move = ""
-        count = 1
         while(move != "End"):
+            #Determina o movimento atual e printa a escolha do agente
             move = agent.generateChoice()
             ambient = np.asarray(copy.deepcopy(agent.ambient.ambient), dtype=object)
             ambient[agent.ambient.agentPos[0], agent.ambient.agentPos[1]] = "x"
             
             os.system('cls')
             print(ambient, "\n", "agent perception: "+str((agent.size[0]+1, agent.size[1]+1)), "\n Action"+str(agent.allChoices[agent.allChoices.__len__()-1]))
+        #Após o agente finalizar de limpar todo o ambiente, o código printa a pontuação 
+         #total do agente e também adiciona a pontuação em "points"
         print("This agent points: "+str(agent.points))
         points.append(agent.points)
-        # print("i = "+str(i))
+        
     maxEncountered.append(max(points))
-# running 100 agents in 'n' threads
-# for i in range(25):
-#     print(i)
-#     threads = []
-#     for i in range(n_threads):
-#         threads.append(Thread(target=runAgent, args=[100]))
-#     for i in range(n_threads):
-#         threads[i].start()
-#     for i in range(n_threads):
-#         threads[i].join()
-runAgent(10) #agents quantity
+
+#Roda a função 10 vezes e informa a maior pontuação encontrada, podendo
+ #ser alterado para a quantidade necessária de agentes
+runAgent(10)
 print(max(maxEncountered))
